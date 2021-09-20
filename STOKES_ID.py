81aa30d658d10bbb95b45ae4f68d63b132165e7c
hrkloeck <46483061+hrkloeck@users.noreply.github.com>
2021-09-20 14:25:46 +0200

    Add files via upload

---
+++ STOKES_ID.py
@@ -0,0 +1,41 @@
+#
+# Copied form 
+#
+# link to different Stoke types
+# https://github.com/SpheMakh/msutils/blob/master/MSUtils/msutils.py
+
+STOKES_TYPES = { 
+    0  : "Undefined", 
+    1  : "I", 
+    2  : "Q", 
+    3  : "U", 
+    4  : "V", 
+    5  : "RR", 
+    6  : "RL", 
+    7  : "LR", 
+    8  : "LL", 
+    9  : "XX", 
+    10 : "XY", 
+    11 : "YX", 
+    12 : "YY", 
+    13 : "RX", 
+    14 : "RY", 
+    15 : "LX", 
+    16 : "LY", 
+    17 : "XR", 
+    18 : "XL", 
+    19 : "YR", 
+    20 : "YL", 
+    21 : "PP", 
+    22 : "PQ", 
+    23 : "QP", 
+    24 : "QQ", 
+    25 : "RCircular", 
+    26 : "LCircular", 
+    27 : "Linear", 
+    28 : "Ptotal", 
+    29 : "Plinear", 
+    30 : "PFtotal", 
+    31 : "PFlinear", 
+    32 : "Pangle", 
+}
